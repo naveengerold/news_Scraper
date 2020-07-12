726a402d35fa693f5ec3cc298b018bd418f99909
naveengerold <47388268+naveengerold@users.noreply.github.com>
2020-07-12 21:21:22 +0530

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,116 @@
+import feedparser as fp
+import numpy as np
+import json
+import newspaper
+from newspaper import Article
+from time import mktime
+from datetime import datetime
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+#%matplotlib inline
+import csv
+
+# Set the limit for number of articles to download
+LIMIT = 100000
+articles_array = []
+
+data = {}
+data['newspapers'] = {}
+
+# Loads the JSON files with news sites
+with open('NewsPapers.json') as data_file:
+    companies = json.load(data_file)
+
+
+
+count = 1
+for company, value in companies.items():
+    if 'rss' in value:
+        d = fp.parse(value['rss'])
+        print("Downloading articles from ", company)
+        newsPaper = {
+            "rss": value['rss'],
+            "link": value['link'],
+            "articles": []
+        }
+        for entry in d.entries:
+            if hasattr(entry, 'published'):
+                if count > LIMIT:
+                    break
+                article = {}
+                article['link'] = entry.link
+                date = entry.published_parsed
+                article['published'] = datetime.fromtimestamp(mktime(date)).isoformat()
+                try:
+                    content = Article(entry.link)
+                    content.download()
+                    content.parse()
+                except Exception as e:
+                    # the next article.
+                    print(e)
+                    print("continuing...")
+                    continue
+                article['title'] = content.title
+                article['text'] = content.text
+                article['authors'] = content.authors
+                #article['top_image'] =  content.top_image
+                #article['movies'] = content.movies
+                newsPaper['articles'].append(article)
+                articles_array.append(article)
+                print(count, "articles downloaded from", company, ", url: ", entry.link)
+                count = count + 1
+    else:
+        print("Building site for ", company)
+        paper = newspaper.build(value['link'], memoize_articles=False)
+        newsPaper = {
+            "link": value['link'],
+            "articles": []
+        }
+        noneTypeCount = 0
+        for content in paper.articles:
+            if count > LIMIT:
+                break
+            try:
+                content.download()
+                content.parse()
+            except Exception as e:
+                print(e)
+                print("continuing...")
+                continue
+            # After 10 downloaded articles from the same newspaper without publish date, the company will be skipped.
+
+            article = {}
+            article['title'] = content.title
+            article['authors'] = content.authors
+            article['text'] = content.text
+            #article['top_image'] =  content.top_image
+            #article['movies'] = content.movies
+            #article['link'] = content.url
+            #article['published'] = content.publish_date
+            newsPaper['articles'].append(article)
+            articles_array.append(article)
+            print(count, "articles downloaded from", company, " using newspaper, url: ", content.url)
+            count = count + 1
+            #noneTypeCount = 0
+    count = 1
+    data['newspapers'][company] = newsPaper
+
+
+
+try:
+    f = csv.writer(open('Scraped_data_news.csv', 'w', encoding='utf-8'))
+    f.writerow(['Title', 'Authors','Text'])
+    print(article)
+    for artist_name in articles_array:
+        title = artist_name['title']
+        authors=artist_name['authors']
+        text=artist_name['text']
+        #image=artist_name['top_image']
+        #video=artist_name['movies']
+        #link=artist_name['link']
+        #publish_date=artist_name['published']
+        # Add each artist’s name and associated link to a row
+        f.writerow([title, authors, text])
+except Exception as e: print(e)
